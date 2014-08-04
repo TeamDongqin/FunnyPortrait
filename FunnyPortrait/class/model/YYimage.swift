966d75509210a635489cea24d57a2ccb2b27c045
dongqin03 <lingshan.zheng.i@icloud.com>
2014-08-04 08:48:54 +0800

    Add YYIMage component for testing

---
+++ FunnyPortrait/class/model/YYimage.swift
@@ -0,0 +1,18 @@
+//
+//  YYimage.swift
+//  FunnyPortrait
+//
+//  Created by zheng lingshan on 14-8-4.
+//  Copyright (c) 2014年 zheng lingshan. All rights reserved.
+//
+
+import UIKit
+import AssetsLibrary
+
+class YYImage : NSObject{
+    var asset:ALAsset!
+    var isSelect:Bool = false
+    init(){
+        super.init()
+    }
+}
---
+++ FunnyPortrait/class/view/YYAlertView.swift
@@ -0,0 +1,9 @@
+//
+//  YYAlertView.swift
+//  FunnyPortrait
+//
+//  Created by zheng lingshan on 14-8-4.
+//  Copyright (c) 2014年 zheng lingshan. All rights reserved.
+//
+
+import Foundation
