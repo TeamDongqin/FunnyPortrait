63e30d24244f7d923c7fa3075016166673edb1b0
dongqin03 <lingshan.zheng.i@icloud.com>
2014-08-05 08:50:57 +0800

    Add YYImage component 2.

--- FunnyPortrait/AppDelegate.swift
+++ FunnyPortrait/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame:UIScreen.mainScreen().bounds)
+        self.window!.backgroundColor = UIColor.whiteColor()
+        self.window!.makeKeyAndVisible()
+        self.window!.rootViewController = MainViewController(nibName:nil, bundle:nil)
+        
         return true
     }
 
